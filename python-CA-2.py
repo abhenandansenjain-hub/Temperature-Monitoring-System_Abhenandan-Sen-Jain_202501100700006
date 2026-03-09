e2386d2380a73c5a4d26814ad1fb8efcc1ae1eaf
Abhenandan Sen Jain <abhenandansenjain@gmail.com>
2026-03-09 16:07:56 +0530

    Update python-CA-2.py

--- python-CA-2.py
+++ python-CA-2.py
@@ -1,4 +1,3 @@
-import random
 import time
 max=int(input("Enter maximum temprature in Celcius : "))
 min=int(input("Enter minimum temprature in Celcius : "))
@@ -9,4 +8,4 @@ for i in range(timt//2):
   print("READING",i," : The temperature is :",float(a),"C")
   mean=mean+a
   time.sleep(2)
-print("Experimental value obtained :",mean/5,"C");
+print("Experimental value obtained :",mean/(timt//2),"C");
