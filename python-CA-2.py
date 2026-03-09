8a0cba1f35bb2b87d0aa0b217dd1c1b0cb6f2426
Abhenandan Sen Jain <abhenandansenjain@gmail.com>
2026-03-09 16:01:56 +0530

    Update python-CA-2.py

--- python-CA-2.py
+++ python-CA-2.py
@@ -6,7 +6,7 @@ timt=int(input("Enter runtime : "))
 mean=0
 for i in range(timt//2):
   a=random.randint(min,max);
-  print("READING",i," : The temprature is :",float(a),"C")
+  print("READING",i," : The temperature is :",float(a),"C")
   mean=mean+a
   time.sleep(2)
 print("Experimental value obtained :",mean/5,"C");
